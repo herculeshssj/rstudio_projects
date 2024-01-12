441639003ea440f71c7bda880ed5f76c0b2d3f04
Hércules S. S. José <hercules_hssj@outlook.com>
2024-01-12 15:10:36 -0300

    Operadores matemáticos

---
+++ operadores_matematicos.R
@@ -0,0 +1,103 @@
+##############################
+### Operadores matemáticos ###
+##############################
+
+### Soma
+5 + 3
+
+### 3 Subtração
+8 - 6
+
+### Multiplicação
+4 * 3
+
+### Divisão
+20 / 5
+
+### Potenciação
+4 ** 2
+# ou
+4 ^ 2
+
+### Radiciação
+# raiz quadrada
+sqrt(81)
+# ou
+81**(1/2) # outra forma de calcular a raiz quadrada: elevar o número a um sobre dois
+
+# raiz cúbica
+125**(1/3)
+27**(1/3)
+
+# Testando a raiz à quarta
+16**4
+65536**(1/4)
+
+### Módulo
+abs(-10)
+abs(10)
+
+
+### Fatorial
+factorial(3) # 3! = 3 * 2 * 1 = 6
+factorial(5) # 5! = 5 * 4 * 3 * 2 * 1 = 120
+factorial(17) # 17! = 17 * 16 * 15 * .... * 2 * 1 = 3.556874e+14
+
+
+### Logaritmo
+# Logaritmo de base 10
+log10(100)
+log10(1000)
+
+# Logaritmo de base 2
+log2(8)
+
+
+### Exponencial
+# e = 2,718281828
+exp(1)
+exp(2)
+exp(-2)
+
+# Logaritmo natural
+log(exp(1)) # Oh preguiça... XD
+log(100)
+
+
+# Número PI
+pi
+
+# pi radianos = 180 graus
+
+
+### Funções trigonométricas
+# No R o resultado das funções trigonométricas é dado em radianos, e não em graus
+
+# Seno
+sin(pi/6) # 30 graus
+asin(0.5) # o valor é pi/6
+
+# Cosseno
+con(pi/4) # pi/4 radianos é igual 45 graus
+acos(0.7071)
+
+# Tangente
+tan(pi/4) # 
+atan(1)
+
+
+# Notação científica
+2e32 # (2 * 10 ^ 32)
+
+2e3 # (2 * 10 ^ 3)
+2 * 10 ^ 3
+
+2e-26
+
+2e5 + 4e5
+
+
+3e20 * 2e12
+
+
+15e16 / 5e6
